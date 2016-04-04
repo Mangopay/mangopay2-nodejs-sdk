f67135ebb1d9655d489253186ee45e72935edbeb
Gabriel Lupu <gabriel.lupu@codegile.com>
2016-04-03 21:40:22 -0400

    Update Users Tests and the server properties extension

--- test/services/Users.js
+++ test/services/Users.js
@@ -1,3 +1,4 @@
+var _ = require('underscore');
 var Promise = require('promise');
 var expect = require('chai').expect;
 var assert = require('chai').assert;
@@ -37,4 +38,57 @@ describe('Users', function() {
             done();
         });
     });
+
+    describe('Get Natural', function() {
+        var john1, john2;
+        before(function(done){
+            Promise.all([api.Users.get(john.Id), api.Users.getNatural(john.Id)]).then(function(res){
+                john1 = res[0];
+                john2 = res[1];
+                done();
+            })
+        });
+
+        it('John should be the same', function(){
+            expect(_.isMatch(john1, john.data)).to.be.true;
+            expect(_.isMatch(john2, john.data)).to.be.true;
+        });
+
+        it('Fails for Legal User', function(done) {
+            api.Users.getNatural(matrix.Id).done(function(){
+                assert.fail('Request passed');
+                done()
+            }, function(){
+                assert.isOk('Should fail');
+                done();
+            });
+        });
+    });
+
+    describe('Get Legal', function(){
+        var matrix1, matrix2;
+        before(function(done){
+            Promise.all([api.Users.get(matrix.Id), api.Users.getLegal(matrix.Id)]).then(function(res){
+                matrix1 = res[0];
+                matrix2 = res[1];
+                done();
+            })
+        });
+
+        it('Matrix should be the same', function(){
+            expect(_.isMatch(matrix1, matrix.data)).to.be.true;
+            expect(_.isMatch(matrix2, matrix.data)).to.be.true;
+        });
+
+        it('Fails for Natural User', function(done) {
+            api.Users.getNatural(matrix.Id).done(function(){
+                assert.fail('Request passed');
+                done()
+            }, function(){
+                assert.isOk('Should fail');
+                done();
+            });
+        });
+    });
+
 });
