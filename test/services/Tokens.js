1e4517aa4d203d5281375e39e13fe2cb086c23ee
Gabriel Lupu <gabriel.lupu@codegile.com>
2016-04-20 18:07:08 -0400

    Add Token Authentication test

---
+++ test/services/Tokens.js
@@ -0,0 +1,21 @@
+var expect = require('chai').expect;
+
+describe('Tokens', function() {
+    var authentication1, authentication2;
+
+    before(function(done){
+        api.authorize(function(data){
+            authentication1 = data;
+            api.authorize(function(data){
+                authentication2 = data;
+                done();
+            });
+        });
+    });
+
+    it('should have different token values', function(){
+        expect(authentication1.access_token).to.not.equal(authentication2.access_token);
+        expect(authentication1.token_type).to.equal('Bearer');
+        expect(authentication2.token_type).to.equal('Bearer');
+    });
+});
