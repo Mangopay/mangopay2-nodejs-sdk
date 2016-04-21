251d07c9b49286a86823ed5de1e5a0b46f2067d3
Gabriel Lupu <gabriel.lupu@codegile.com>
2016-04-20 21:50:59 -0400

    Add Hooks test

--- test/services/Hooks.js
+++ test/services/Hooks.js
@@ -14,10 +14,54 @@ describe('Hooks', function() {
 
     describe('Create', function () {
         before(function (done) {
+            api.Hooks.getAll(function(data, response){
+                if (data.length) {
+                    hook = data[0];
+                    done();
+                } else {
+                    api.Hooks.create({
+                        EventType: 'PAYIN_NORMAL_CREATED',
+                        Url: 'http://test.com'
+                    }, function(data, response){
+                        hook = data;
+                        done()
+                    });
+                }
+            });
         });
 
-        it('should create', function () {
-            expect().to.equal();
+        it('should be created', function () {
+            expect(hook.Id).to.exist;
+        });
+    });
+
+    describe('Get', function () {
+        var getHook;
+        before(function (done) {
+            api.Hooks.get(hook.Id, function(data, response){
+                getHook = data;
+                done();
+            });
+        });
+
+        it('should be the same as the created one', function () {
+            expect(getHook.Id).to.equal(hook.Id);
+        });
+    });
+
+    describe('Update', function () {
+        var updatedHook;
+        var newUrl = 'http://test123.com?_t=' + new Date().getTime();
+        before(function (done) {
+            hook.Url = newUrl;
+            api.Hooks.update(hook, function(data, response){
+                updatedHook = data;
+                done();
+            });
+        });
+
+        it('should be updated', function () {
+            expect(updatedHook.Url).to.equal(newUrl);
         });
     });
 });
