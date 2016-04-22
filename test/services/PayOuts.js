95658d946754da1dd56f7b48dbba517c0d83c16f
Gabriel Lupu <gabriel.lupu@codegile.com>
2016-04-21 22:39:40 -0400

    PayOuts tests

---
+++ test/services/PayOuts.js
@@ -0,0 +1,45 @@
+var expect = require('chai').expect;
+
+var helpers = require('../helpers');
+
+describe('PayOuts', function() {
+    var john = helpers.data.UserNatural;
+    var payOut;
+
+    before(function(done){
+        api.Users.create(john, function(){
+            done();
+        });
+    });
+
+    describe('Create', function() {
+        before(function(done){
+            helpers.getNewPayoutBankWire(api, john, function(data, response){
+                payOut = data;
+                done();
+            });
+        });
+
+        it('should be created', function(){
+            expect(payOut.Id).to.exist;
+            expect(payOut.PaymentType).to.equal('BANK_WIRE');
+        });
+    });
+
+    describe('Get', function() {
+        var getPayOut;
+
+        before(function(done){
+            api.PayOuts.get(payOut.Id, function(data, response){
+                getPayOut = data;
+                done();
+            });
+        });
+
+        it('should be fetched', function(){
+            expect(payOut.Id).to.equal(getPayOut.Id);
+            expect(payOut.Status).to.equal('FAILED');
+            expect(payOut.ExecutionDate).to.be.null;
+        });
+    });
+});
