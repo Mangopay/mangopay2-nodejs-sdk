3218753e1a16dffc4d2b09715ee6ab76ec021e90
Paul Hristea <paul.hristea@codegile.com>
2017-08-12 14:41:03 +0300

    Reverted some changes in an example file.

--- examples/getUserEmoney.js
+++ examples/getUserEmoney.js
@@ -2,24 +2,14 @@
  * Examples of getting a user's emoney
  */
 var mangopay = require('../index');
-var PayIn = require('../lib/models/PayIn')
 
 var api = new mangopay({
-    baseUrl: 'https://api-test.mangopay.com/',
     clientId: 'sdk-unit-tests',
-    clientPassword: '9RMGpwVUwFLK0SurxObJ2yaadDcO0zeKFKxWmthjB93SQjFzy0'
-    //cqFfFrWfCcb7UadHNxx2C9Lo6Djw8ZduLi7J9USTmu8bhxxpju
-    //9RMGpwVUwFLK0SurxObJ2yaadDcO0zeKFKxWmthjB93SQjFzy0
+    clientPassword: 'cqFfFrWfCcb7UadHNxx2C9Lo6Djw8ZduLi7J9USTmu8bhxxpju'
 });
 
-var USER_ID = '1151091';
+var USER_ID ='1151091';
 
-api.PayIns.get('2826947').then(function(data){
-    var payIn = new PayIn(data);
-    console.log(payIn.ExecutionType);
-})
-
-// api.Users.getEMoney(USER_ID).then(function(data){
-//     console.log('hi')
-//     console.log(data);
-// });
+api.Users.getEMoney(USER_ID).then(function(data){
+    console.log(data);
+});
