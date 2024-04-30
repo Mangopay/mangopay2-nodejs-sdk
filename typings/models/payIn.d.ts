8e4ea53663c68e6bb3799121a1d52a40acc0b737
Iulian Masar <iulian.masar@codegile.com>
2024-04-30 13:24:18 +0300

    updated PaymentMethodMetadata

--- typings/models/payIn.d.ts
+++ typings/models/payIn.d.ts
@@ -1982,6 +1982,16 @@ export namespace payIn {
          * The card brand. Examples include: AMERICAN EXPRESS, DISCOVER, JCB, MASTERCARD, VISA, etc.
          */
         Brand: string;
+
+        /**
+         * Whether the card is held in a personal or commercial capacity.
+         */
+        CommercialIndicator: string;
+
+        /**
+         * The type of the card. Allowed / Returned / Default values: CREDIT, DEBIT, CHARGE CARD
+         */
+        CardType: string;
     }
 
     interface PaymentMethodMetadata {
@@ -1989,36 +1999,34 @@ export namespace payIn {
          * The type of metadata. Allowed values: BIN, GOOGLE_PAY
          */
         Type: string;
+
         /**
          * The bank identification number (BIN). (Format: 6 or 8 digits)
          */
         Bin: string;
+
         /**
          * The tokenized payment data provided by the third-party payment method.
          */
         Token: string;
+
         /**
          * In the case of Google Pay, the format of the Token.
          * PAN_ONLY – The card is registered in the Google account and requires 3DS authentication.
          * CRYPTOGRAM_3DS – The card is enrolled in the customer’s Google Wallet and authentication is handled by the Android device.
          */
         TokenFormat: string;
-        /**
-         * The type of the card. Allowed / Returned / Default values: CREDIT, DEBIT, CHARGE CARD
-         */
-        CardType: string;
+
         /**
          * The country where the card was issued. Format: ISO-3166 alpha-2 two-letter country code
          */
         IssuerCountryCode: string;
+
         /**
          * The name of the card issuer.
          */
         IssuingBank: string;
-        /**
-         * Whether the card is held in a personal or commercial capacity.
-         */
-        CommercialIndicator: string;
+
         /**
          * Additional data about the card based on the BIN. In the case of co-branded card products, two objects are returned.
          */
@@ -2030,10 +2038,12 @@ export namespace payIn {
          * The type of metadata. Allowed values: BIN, GOOGLE_PAY
          */
         Type: string;
+
         /**
          * The bank identification number (BIN). (Format: 6 or 8 digits)
          */
         Bin?: string;
+
         /**
          * The tokenized payment data provided by the third-party payment method.
          */
