95f51efb0013918a5642c0ea65c0856ba90c7cd4
Farah Aly <farah.aly@mangopay.com>
2024-07-11 16:30:08 +0100

    Add StatementDescriptor parameter to Refund

--- typings/models/refund.d.ts
+++ typings/models/refund.d.ts
@@ -46,6 +46,8 @@ export namespace refund {
         DebitedFunds?: MoneyData;
 
         Fees?: MoneyData;
+        
+        StatementDescriptor?: string;
     }
 
     interface CreateTransferRefund {
