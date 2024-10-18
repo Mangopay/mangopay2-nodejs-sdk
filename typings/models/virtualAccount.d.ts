75c87f40d843602dea903fe52bc6c687435d2295
Iulian Masar <iulian.masar@codegile.com>
2024-10-18 14:03:58 +0300

    added docs

--- typings/models/virtualAccount.d.ts
+++ typings/models/virtualAccount.d.ts
@@ -4,21 +4,76 @@ import { entityBase } from "./entityBase";
 export namespace virtualAccount {
 
     interface VirtualAccountData extends entityBase.EntityBaseData {
+        /**
+         * The wallet ID
+         */
         WalletId: string;
+
+        /**
+         * The credited user ID
+         */
         CreditedUserId: string;
+
+        /**
+         * The type of the virtual account
+         * Allowed values: `COLLECTION`, `USER_OWNED`
+         */
         VirtualAccountPurpose: VirtualAccountPurpose;
+
+        /**
+         * The country of the IBAN. The country must correspond to the currency of the wallet
+         */
         Country: CountryISO;
+
+        /**
+         * The status of the virtual account creation
+         */
         Status: VirtualAccountStatus;
+
+        /**
+         * Whether the virtual account is active and can receive pay-ins. `Active` is only `true`
+         * when the `Status` is `ACTIVE`.
+         */
         Active: boolean;
+
+        /**
+         * Owner of the virtual account.
+         */
         AccountOwner: VirtualAccountOwner;
+
+        /**
+         * Details of the account in local account identifier format
+         */
         LocalAccountsDetails: LocalAccountsDetails;
+
+        /**
+         * Details of the account in IBAN format.
+         * The `InternationalAccountDetails` are not returned if the country doesn’t use IBAN (e.g. AU -
+         * Australia)
+         */
         InternationalAccountDetails: InternationalAccountDetails[];
+
+        /**
+         * Information about the account’s payment capabilities
+         */
         Capabilities: VirtualAccountCapabilities;
     }
 
     interface CreateVirtualAccount {
+        /**
+         * The country of the IBAN. The country must correspond to the currency of the wallet
+         */
         Country: CountryISO;
+
+        /**
+         * The type of the virtual account
+         * Allowed values: `COLLECTION`, `USER_OWNED`
+         */
         VirtualAccountPurpose: VirtualAccountPurpose;
+
+        /**
+         * Tag
+         */
         Tag?: string;
     }
 
@@ -34,13 +89,31 @@ export namespace virtualAccount {
     }
 
     interface InternationalAccountDetails {
+        /**
+         * Information about the address associated with the international IBAN account.
+         */
         Address: VirtualAccountAddress;
+
+        /**
+         * The IBAN and BIC of the account.
+         */
         Account: Account;
     }
 
     interface LocalAccountsDetails {
+        /**
+         * Information about the address associated with the local IBAN account.
+         */
         Address: VirtualAccountAddress;
+
+        /**
+         * The account number of the account
+         */
         AccountNumber: string;
+
+        /**
+         * The sort code of the account.
+         */
         SortCode: string;
     }
 
@@ -58,8 +131,19 @@ export namespace virtualAccount {
     }
 
     interface VirtualAccountCapabilities {
+        /**
+         * Whether or not local bank wires can be made to this account.
+         */
         LocalPayinAvailable: boolean;
+
+        /**
+         * Whether or not international bank wires can be made to this account
+         */
         InternationalPayinAvailable: boolean;
+
+        /**
+         * List of currencies supported by the account
+         */
         Currencies: CurrencyISO[];
     }
 }
