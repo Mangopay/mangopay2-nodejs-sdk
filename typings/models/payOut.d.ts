f29b3573d8fcfd70475cc94511b5c92a14267ba9
Iulian <40763147+iulian03@users.noreply.github.com>
2025-04-16 13:39:07 +0300

    [feature] twint payin (#452)
    * update PayoutModeRequestedType and added PayoutModeAppliedType
    
    * added support for Twint PayIn
    
    * reverted payout change
    
    ---------
    
    Co-authored-by: Iulian Masar <iulian.masar@codegile.com>

--- typings/models/payOut.d.ts
+++ typings/models/payOut.d.ts
@@ -32,7 +32,7 @@ export namespace payOut {
         /**
          * Payment reference provided for the payout.
          */
-        PaymentRef: PayoutPaymentRefData
+        PaymentRef: PayoutPaymentRefData;
     }
 
     interface CreatePayOut {
