3e5bbbf77b5e51e79ddf804a65dc31da4845418a
Iulian <40763147+iulian03@users.noreply.github.com>
2025-04-16 13:44:02 +0300

    [feature] updated PayoutModeRequestedType and added PayoutModeAppliedType (#451)
    * update PayoutModeRequestedType and added PayoutModeAppliedType
    
    * added missing ';'
    
    ---------
    
    Co-authored-by: Iulian Masar <iulian.masar@codegile.com>

--- typings/models/payOut.d.ts
+++ typings/models/payOut.d.ts
@@ -9,7 +9,8 @@ export namespace payOut {
     import FallbackReasonData = base.FallbackReasonData;
     import PayoutPaymentRefData = base.PayoutPaymentRefData;
 
-    type PayoutModeRequestedType = "STANDARD" | "INSTANT_PAYMENT" | "INSTANT_PAYMENT_ONLY";
+    type PayoutModeRequestedType = "STANDARD" | "INSTANT_PAYMENT" | "INSTANT_PAYMENT_ONLY" | "RTGS_PAYMENT";
+    type PayoutModeAppliedType = "STANDARD" | "INSTANT_PAYMENT" | "RTGS_PAYMENT" | "PENDING_RESPONSE";
 
     interface PayOutData extends Omit<transfer.TransferData, "Type"> {
         /**
@@ -33,6 +34,10 @@ export namespace payOut {
          * Payment reference provided for the payout.
          */
         PaymentRef: PayoutPaymentRefData;
+
+        ModeRequested: PayoutModeRequestedType;
+
+        ModeApplied: PayoutModeAppliedType;
     }
 
     interface CreatePayOut {
