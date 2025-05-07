9d816b9f0fe8a23845b9fa9632f270f57badb60d
Iulian Masar <iulian.masar@codegile.com>
2025-05-07 14:15:59 +0300

    updated doc

--- typings/models/identityVerification.d.ts
+++ typings/models/identityVerification.d.ts
@@ -50,7 +50,6 @@ export namespace identityVerification {
     interface CheckData {
         /**
          * The type of the data point.
-         * For more details, <a href="https://mangopay-idv.mintlify.app/guides/users/verification/hosted?_gl=1*1unwn0t*_up*MQ..*_ga*ODg5MjI4ODQzLjE3Mzg5MjY2NjE.*_ga_VZLQHP6CFB*MTczODkyNjY2MC4xLjAuMTczODkyNjY2MC4wLjAuMA..#verified-data-returned">see the Verified data returned.</a>
          */
         Type: string;
 
